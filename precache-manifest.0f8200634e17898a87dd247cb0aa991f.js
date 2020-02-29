14a4c9e3a914f82c1af896d873f93ab9a3fc23aa
Anna Kholod <uashop89@gmail.com>
2020-02-29 12:03:27 +0200

    Updates

---
+++ precache-manifest.0f8200634e17898a87dd247cb0aa991f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d424bcc7c85a792319e0f63feaac86",
+    "url": "/pro-fitness/index.html"
+  },
+  {
+    "revision": "06edd2b8a12503087e2f",
+    "url": "/pro-fitness/static/css/main.718c83bf.chunk.css"
+  },
+  {
+    "revision": "c1a3bcda39f4d262e5e4",
+    "url": "/pro-fitness/static/js/2.3bfd6ed6.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/pro-fitness/static/js/2.3bfd6ed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06edd2b8a12503087e2f",
+    "url": "/pro-fitness/static/js/main.b482de9d.chunk.js"
+  },
+  {
+    "revision": "c0d2bc8161c310f9f216",
+    "url": "/pro-fitness/static/js/runtime-main.7e0bf87d.js"
+  },
+  {
+    "revision": "a63d5ec46223247265078efa303cad33",
+    "url": "/pro-fitness/static/media/about.a63d5ec4.jpg"
+  },
+  {
+    "revision": "89df21cad0690b924db667f91d0ca736",
+    "url": "/pro-fitness/static/media/first-lesson.89df21ca.jpg"
+  },
+  {
+    "revision": "594557fd828c6d72845af11d7d78c293",
+    "url": "/pro-fitness/static/media/fitness-1400.594557fd.jpg"
+  },
+  {
+    "revision": "3e6113c95405acc2fef94bec82a8c8b3",
+    "url": "/pro-fitness/static/media/fitness-768.3e6113c9.jpg"
+  },
+  {
+    "revision": "1df8f8900eb12aedaa8c94f17d261b43",
+    "url": "/pro-fitness/static/media/footer.1df8f890.jpg"
+  },
+  {
+    "revision": "32be801651c68ef04161d0910bc7af4d",
+    "url": "/pro-fitness/static/media/news.32be8016.jpg"
+  }
+]);
